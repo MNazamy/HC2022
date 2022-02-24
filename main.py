5c05112281bf3dfe8971640335558acb96def1d4
Michael Grossman <michael.grossman066@qmail.cuny.edu>
2022-02-24 15:01:01 -0500

    edits to main

--- main.py
+++ main.py
@@ -67,12 +67,20 @@ def readInput(filename):
                 requiredLvl = int(words[1])
                 temp.addRole(skillNeeded, requiredLvl)
 
-            projects.append((temp.sortingValue ,temp))
+            projects.append([temp.sortingValue(0) ,temp])
     return contributors, projects, skd
 
     
 
-
+def output(filename, pcount, plist):
+    with open(filename, "w") as file:
+        file.write(pcount)
+        for _ in plist:
+            file.write(_.name)
+            s = ""
+            for person in _.contributorList:
+                s += person.name + " "
+            file.write(s)
 
 
 
@@ -86,11 +94,56 @@ def run(fileName):
     #priority queue of projects
     projectPQ = []
 
+    #list holding project for later execution
+    laterList = []
+
+    #priority queue for current projects
+    currPrjPQ = []
+
+    score = 0
+
+    projectOrder = []
+
 
     peopleAvailable, projectPQ, skillDict = readInput(fileName)
 
     heapq.heapify(projectPQ)
 
+    day = 0
+    projectCount = 0
+    projectsWorthPointsAreAvailable = True
+    numPeopleAvail = len(peopleAvailable)
+
+    while projectsWorthPointsAreAvailable:
+        for _ in projectPQ:
+            _[0] = _[1].sortingValue(day)
+
+        heapq.heapify(projectPQ)
+
+        projectAssigned = -1
+        while(len(projectPQ) > 0 and numPeopleAvail > 0 and projectPQ[0][0] > 0):
+            currPrj = heapq.heappop(projectPQ)
+            projectAssigned, numPeopleAvail = checkProject(currPrj[1], peopleAvailable, skillDict, numPeopleAvail)
+            if projectAssigned == -1:
+                laterList.append(currPrj)
+            else:
+                currPrjPQ.append([currPrj.numberOfDays-1, currPrj[1]])
+                projectOrder.append(currPrj[1])
+        
+        projectPQ += laterList
+        heapq.heapify(projectPQ)
+        heapq.heapify(currPrjPQ)
+
+        while(currPrjPQ[0][0] == day):
+            currPrj = heapq.heappop(currPrjPQ)[1]
+            score += currPrj.finishProject(day)
+            projectCount += 1
+        
+        projectsWorthPointsAreAvailable = projectPQ[0][0] > 0
+
+        day += 1
+
+    output("output.txt", projectCount, projectOrder)
 
 
     
