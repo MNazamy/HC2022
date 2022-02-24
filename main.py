0845ac0df27d7627f893fa829b58e7c040f0e2fb
Matin Nazamy <matin.nazamy50@qmail.cuny.edu>
2022-02-23 21:03:47 -0500

    Preparing

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+def run(filename):
+    
+    with open(filename, "r") as inFile:
+
+        firstLine = inFile.readline()
+        secondLine = inFile.readline()
+        
+
+        
+
+
+
+if __name__ == "__main__":
+        run("input.txt")
